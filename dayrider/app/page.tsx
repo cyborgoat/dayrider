89a22167ae07299556d143851f01cb6c357379b6
cyborgoat <cyborgoat@outlook.com>
2023-11-05 14:48:42 +0800

    updated: dayrider

--- dayrider/app/page.tsx
+++ dayrider/app/page.tsx
@@ -1,16 +1,12 @@
-'use client'
-import Image from 'next/image'
+import Image from "next/image";
 import Link from "next/link";
-import {Button, NextUIProvider} from "@nextui-org/react";
+import { Button, NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
-    return (
-        <NextUIProvider>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Button>Press me</Button>
-            <Link href={'./schedule'}>Go to schedule</Link>
-        </main>
-        </NextUIProvider>
-
-    )
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Press me</Button>
+      <Link href={"./schedule"}>Go to schedule</Link>
+    </main>
+  );
 }
