feb60eafdd60aaacf9135540df9b771548f19cc1
cyborgoat <cyborgoat@outlook.com>
2024-07-13 16:39:59 +0800

    updated: sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,62 +2,52 @@
 import {Divider, Listbox, ListboxItem} from "@nextui-org/react";
 import TodoIcon from "@/components/icons/TodoIcon";
 import {CalendarIcon} from "@/components/icons/CalendarIcon";
-import {useState} from "react";
+import React, {useState} from "react";
 import {SettingsIcon} from "@/components/icons/SettingsIcon";
 import {PersonIcon} from "@/components/icons/PersonIcon";
+import {FaRegUserCircle, FaTasks} from "react-icons/fa";
+import {PiList, PiListChecksFill} from "react-icons/pi";
+import {FaListCheck} from "react-icons/fa6";
+import {BsCalendar2Check, BsCalendar2Day} from "react-icons/bs";
+import {IoIosSettings} from "react-icons/io";
 
 export default function SideBar() {
-  const [titleColor, setTitleColor] = useState("text-sky-500");
+    const [titleColor, setTitleColor] = useState("text-sky-500");
 
-  return (
-    <div className="sticky top-0 h-screen max-w-md pt-4 mx-4">
-      <div>
-        <a href={"/"} className={`text-lg font-medium`}>
-          DayRiderV2
-        </a>
-        <p className="text-sm text-default-400">
-          Your personal intelligent assistant.
-        </p>
-      </div>
-      <Divider className="mt-4" />
-      <span className="pt-4 text-xs font-bold text-stone-700/40">Apps</span>
-      <div className="w-full pb-2 border-small rounded-small border-default-200 dark:border-default-100">
-        <Listbox
-          aria-label="Actions"
-          onAction={(key) => setTitleColor("text-slate-900")}
-        >
-          <ListboxItem key="todo" href={"/todo"} textValue={"todo"}>
-            <div className="flex flex-row">
-              <TodoIcon /> <span className="ml-1 text-md">Todo</span>
+    return (
+        <div className="sticky top-0 h-screen max-w-md pt-4 mx-2">
+            <div>
+                <a href={"/"} className={`text-md font-medium`}>
+                    DayRiderV2
+                </a>
             </div>
-          </ListboxItem>
-          <ListboxItem key="schedule" href={"/schedule"} textValue={"schedule"}>
-            <div className="flex flex-row">
-              <CalendarIcon /> <span className="ml-1 text-md">Schedule</span>
+            <Divider className="mt-4"/>
+            <div className="w-full pb-2 border-small rounded-small border-default-200 dark:border-default-100">
+                <Listbox aria-label="Actions" onAction={(key) => setTitleColor("text-slate-900")}>
+                    <ListboxItem
+                        key="todo" href={"/todo"} textValue={"todo"}
+                        startContent={<FaListCheck strokeWidth={0.3} size={24}/>}
+                    >
+                        <span className="text-md font-[500]">Todo</span>
+                    </ListboxItem>
+                    <ListboxItem key="schedule" href={"/schedule"} textValue={"schedule"}
+                                 startContent={<BsCalendar2Day strokeWidth={0.3} size={24}/>}
+                    >
+                        <span className="text-md font-[500]">Schedule</span>
+                    </ListboxItem>
+                    <ListboxItem key="schedule" href={"/profile"} textValue={"schedule"}
+                                 startContent={<FaRegUserCircle strokeWidth={0.3} size={24}/>}
+                    >
+                        <span className="text-md font-[500]">Profile</span>
+                    </ListboxItem>
+                    <ListboxItem key="settings" href={"/settings"} textValue={"settings"}
+                                 startContent={<IoIosSettings strokeWidth={0.3} size={24}/>}
+                    >
+                        <span className="text-md font-[500]">Settings</span>
+                    </ListboxItem>
+                </Listbox>
             </div>
-          </ListboxItem>
-        </Listbox>
-      </div>
-      <div
-        className="w-full border-small py-2
-  rounded-small border-default-200 dark:border-default-100"
-      >
-        <Listbox
-          aria-label="Actions"
-          onAction={(key) => setTitleColor("text-slate-900")}
-        >
-          <ListboxItem key="schedule" href={"/profile"} textValue={"schedule"}>
-            <div className="flex flex-row">
-              <PersonIcon /> <span className="ml-1 text-md">My Profile</span>
-            </div>
-          </ListboxItem>
-          <ListboxItem key="settings" href={"/settings"} textValue={"settings"}>
-            <div className="flex flex-row">
-              <SettingsIcon /> <span className="ml-1 text-md">Settings</span>
-            </div>
-          </ListboxItem>
-        </Listbox>
-      </div>
-    </div>
-  );
+
+        </div>
+    );
 }
