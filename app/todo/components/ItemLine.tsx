a2863fc428244af8e3508c04d94d0c472a8a7382
cyborgoat <cyborgoat@outlook.com>
2024-07-21 01:14:44 +0800

    updated: todos

--- app/todo/components/ItemLine.tsx
+++ app/todo/components/ItemLine.tsx
@@ -4,31 +4,33 @@ import {Input} from "@nextui-org/react";
 import {useState} from "react";
 
 const ItemLine = (props: { todo: TodoItem }) => {
-  const [isFocused, setIsFocused] = useState(false);
-  const [value, setValue] = useState(props.todo.name);
+    const [todo, setTodo] = useState(props.todo);
+    const [isFocused, setIsFocused] = useState(false);
 
-  return (
-    <div className="w-full py-0">
-      <Input
-        type="text"
-        variant="flat"
-        label=""
-        defaultValue={value}
-        onFocus={(e) => {
-          console.log("focused");
-          setIsFocused(true);
-        }}
-        onBlur={() => setIsFocused(false)}
-      />
-      <div
-        className={`transition-all ease-in mx-3 overflow-hidden ${
-          isFocused ? "duration-500 max-h-32" : "duration-0 max-h-0 invisible"
-        }`}
-      >
-        dueOn:{props.todo.dueOn}
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full py-0">
+            <Input
+                type="text"
+                variant="flat"
+                label=""
+                defaultValue={todo.name}
+                onFocus={(e) => {
+                    console.log("focused");
+                    setIsFocused(true);
+                }}
+                onBlur={() => setIsFocused(false)}
+                onChange={e => setTodo({...todo, name: e.target.value})}
+            />
+            <div
+                className={`transition-all ease-in mx-3 overflow-hidden ${
+                    isFocused ? "duration-500 max-h-32" : "duration-0 max-h-0 invisible"
+                }`}
+            >
+                name:{todo.name}
+                dueOn:{todo.dueOn}
+            </div>
+        </div>
+    );
 };
 
 export default ItemLine;
