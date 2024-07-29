226d44a28e93a3da23106e5b6f45639b5b554616
cyborgoat <cyborgoat@outlook.com>
2024-07-29 23:04:18 +0800

    updated: add button

--- app/todo/components/DeletePopover.tsx
+++ app/todo/components/DeletePopover.tsx
@@ -1,36 +1,57 @@
-import {Listbox, ListboxItem, Popover, PopoverContent, PopoverTrigger} from "@nextui-org/react";
+import {
+  Listbox,
+  ListboxItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
 import React from "react";
-import {onItemRemoveFunction, TodoItem} from "@/app/todo/types/todoItem";
-import {ListboxWrapper} from "@/components/ListboxWrapper";
-import {CustomizedButton} from "@/app/todo/components/CustomizedTypes";
-import {FiTrash2} from "react-icons/fi";
-import {IoCheckmarkSharp} from "react-icons/io5";
+import { onItemRemoveFunction, TodoItem } from "@/app/todo/types/todoItem";
+import { ListboxWrapper } from "@/components/ListboxWrapper";
+import { CustomizedButton } from "@/app/todo/components/CustomizedTypes";
+import { FiTrash2 } from "react-icons/fi";
+import { IoCheckmarkSharp } from "react-icons/io5";
 
-export default function DeletePopover(props: { todo: TodoItem, onItemRemove: onItemRemoveFunction }
-) {
-    const [isOpen, setIsOpen] = React.useState(false);
-    return (
-        <Popover showArrow placement="bottom" isOpen={isOpen} onOpenChange={setIsOpen}>
-            <PopoverTrigger className="">
-                <CustomizedButton color="trash" radius="full" isIconOnly size={"tiny"}>
-                    <FiTrash2 size={18}/>
-                </CustomizedButton>
-            </PopoverTrigger>
-            <PopoverContent className="min-w-[100px] p-1">
-                <ListboxWrapper>
-                    <Listbox
-                        aria-label="Actions"
-                        onAction={(key) => alert(key)}
-                    >
-                        <ListboxItem className="text-danger" key="delete"
-                                     onClick={() => {
-                                         props.onItemRemove(props.todo.uuid)
-                                     }}
-                                     endContent={<IoCheckmarkSharp/>}>Delete</ListboxItem>
-                        <ListboxItem key="cancel" onClick={() => setIsOpen(false)}>Cancel</ListboxItem>
-                    </Listbox>
-                </ListboxWrapper>
-            </PopoverContent>
-        </Popover>
-    );
+export default function DeletePopover(props: {
+  todo: TodoItem;
+  onItemRemove: onItemRemoveFunction;
+}) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <Popover
+      showArrow
+      placement="bottom"
+      isOpen={isOpen}
+      onOpenChange={setIsOpen}
+    >
+      <PopoverTrigger className="">
+        <CustomizedButton color="trash" radius="full" isIconOnly size={"tiny"}>
+          <FiTrash2 size={18} />
+        </CustomizedButton>
+      </PopoverTrigger>
+      <PopoverContent className="min-w-[80px] p-0">
+        <ListboxWrapper>
+          <Listbox aria-label="Actions" onAction={(key) => alert(key)}>
+            <ListboxItem
+              className="text-danger text-center"
+              key="delete"
+              onClick={() => {
+                props.onItemRemove(props.todo.uuid);
+                setIsOpen(false);
+              }}
+            >
+              Delete
+            </ListboxItem>
+            <ListboxItem
+              key="cancel"
+              className="text-center"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </ListboxItem>
+          </Listbox>
+        </ListboxWrapper>
+      </PopoverContent>
+    </Popover>
+  );
 }
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,52 +1,67 @@
 "use client";
-import React, {useEffect, useState} from "react";
-import {TodoItem} from "@/app/todo/types/todoItem";
+import React, { useEffect, useState } from "react";
+import { TodoItem } from "@/app/todo/types/todoItem";
 import ItemLine from "./components/ItemLine";
-import {addTodoItem, defaultNewItem, deleteTodoItem, getTodoItems} from "@/app/todo/lib/utils";
-
+import {
+  addTodoItem,
+  defaultNewItem,
+  deleteTodoItem,
+  getTodoItems,
+} from "@/app/todo/lib/utils";
+import { Button } from "@nextui-org/react";
+import { FaSackDollar } from "react-icons/fa6";
+import { IoIosAdd } from "react-icons/io";
 
 export default function TodoPage() {
-    const [todoList, setTodoList] = useState<TodoItem[] | undefined>();
+  const [todoList, setTodoList] = useState<TodoItem[] | undefined>();
 
-    useEffect(() => {
-        if (todoList === undefined) {
-            getTodoItems().then(result => {
-                setTodoList(result)
-            }).catch(e => console.log(e))
+  useEffect(() => {
+    if (todoList === undefined) {
+      getTodoItems()
+        .then((result) => {
+          setTodoList(result);
+        })
+        .catch((e) => console.log(e));
+    }
+  }, [todoList]);
+
+  const onItemAdd = (e: any) => {
+    addTodoItem(defaultNewItem())
+      .then((item) => setTodoList([...(todoList ? todoList : []), item]))
+      .catch((e) => console.log(e));
+  };
+
+  const onItemRemove = (uuid: string) => {
+    deleteTodoItem(uuid)
+      .then((res) => {
+        if (todoList !== undefined) {
+          setTodoList(todoList.filter((item) => item.uuid !== uuid));
         }
-    }, [todoList])
+      })
+      .catch((e) => console.log(e));
+  };
 
-    const onItemAdd = (e: any) => {
-        addTodoItem(defaultNewItem()).then(item =>
-            setTodoList([
-                ...todoList ? todoList : [],
-                item
-            ])
-        ).catch(e => console.log(e));
-    }
-
-    const onItemRemove = (uuid: string) => {
-        deleteTodoItem(uuid).then(res => {
-                if (todoList !== undefined) {
-                    setTodoList(todoList.filter(item => item.uuid !== uuid))
-                }
-            }
-        ).catch(e => console.log(e));
-
-
-    }
-
-    return (
-        <main className="flex flex-col items-start min-h-screen px-4 pt-6 justify-items-start lg:px-6">
-            <div className={"text-2xl font-semibold text-blue-500 mb-2"}>Todos</div>
-            <div className="flex flex-col gap-y-2 my-4 w-full">
-                {todoList?.map((todo, idx) => (
-                    <ItemLine todo={todo} key={`item-${idx}`} onItemRemove={onItemRemove}/>
-                ))}
-            </div>
-            <button className="text-blue-500 text-md font-semibold pl-2" onClick={onItemAdd}>+ Add another todo
-                item
-            </button>
-        </main>
-    );
+  return (
+    <main className="flex flex-col items-start min-h-screen px-4 pt-6 justify-items-start lg:px-6">
+      <div className="w-full flex flex-row justify-between">
+        <div className={"text-2xl font-semibold text-blue-500 mb-2"}>Todos</div>
+        <Button variant="light" isIconOnly onClick={onItemAdd}> <IoIosAdd size={24} /> </Button>
+      </div>
+      <div className="flex flex-col gap-y-2 my-4 w-full">
+        {todoList?.map((todo, idx) => (
+          <ItemLine
+            todo={todo}
+            key={`item-${idx}`}
+            onItemRemove={onItemRemove}
+          />
+        ))}
+      </div>
+      <button
+        className="text-blue-500 text-md font-semibold pl-2"
+        onClick={onItemAdd}
+      >
+        + Add another todo item
+      </button>
+    </main>
+  );
 }
