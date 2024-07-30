69e0ecbfc33d19df328847139eea436d02f3e3c9
cyborgoat <cyborgoat@outlook.com>
2024-07-30 22:50:49 +0800

    fixed: window popup bug

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use todo::db;
 use todo::greet;
 
 mod todo;
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
