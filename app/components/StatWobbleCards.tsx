f2a1f5b48c606b1f89301695bdf34f731bddfbfa
cyborgoat <cyborgoat@outlook.com>
2024-09-16 21:32:36 +0800

    updated styles

--- app/components/StatWobbleCards.tsx
+++ app/components/StatWobbleCards.tsx
@@ -30,7 +30,7 @@ export function StatWobbleCards() {
 
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-11/12">
             <WobbleCard
                 containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
                 className=""
