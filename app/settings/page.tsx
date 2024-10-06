ddae041f9f29259fa06211326f44edd083452eba
cyborgoat <cyborgoat@outlook.com>
2024-10-06 10:47:42 +0800

    feat: priority settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         <>
             <div className="mx-auto max-w-7xl lg:flex lg:gap-x-16 lg:px-8">
                 <main className="px-4 py-4 sm:px-6 lg:flex-auto lg:px-0 lg:py-6">
-                    <div className="max-w-2xl mx-auto space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
+                    <div className="max-w-3xl mx-auto space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
                         <div>
                             <h2 className="text-base font-semibold text-gray-900 leading-7">
                                 Profile
@@ -49,16 +49,14 @@ export default function Page() {
                                     <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
                                         Full Name
                                     </dt>
-                                    <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">
-                                        <input
-                                            className="text-gray-900 hover:bg-slate-400/40 px-1"
-                                            defaultValue={userName}
-                                            onChange={(e) => setUserName(e.target.value)}
-                                            onBlur={(e) => {
-                                                setUser(userName, userId, userEmail).then().catch(e => console.log(e));
-                                            }}
-                                        />
-                                    </dd>
+                                    <input
+                                        className="border-1.5 rounded-md text-gray-900 hover:bg-slate-400/40 px-1"
+                                        defaultValue={userName}
+                                        onChange={(e) => setUserName(e.target.value)}
+                                        onBlur={(e) => {
+                                            setUser(userName, userId, userEmail).then().catch(e => console.log(e));
+                                        }}
+                                    />
                                 </div>
                                 <div className="pt-6 sm:flex">
                                     <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
@@ -66,7 +64,7 @@ export default function Page() {
                                     </dt>
                                     <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">
                                         <input
-                                            className="text-gray-900 hover:bg-slate-400/40 px-1"
+                                            className="border-1.5 rounded-md text-gray-900 hover:bg-slate-400/40 px-1"
                                             defaultValue={userEmail}
                                             onChange={(e) => setUserEmail(e.target.value)}
                                             onBlur={(e) => {
@@ -81,7 +79,7 @@ export default function Page() {
                                     </dt>
                                     <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">
                                         <input
-                                            className="text-gray-900 hover:bg-slate-400/40 px-1"
+                                            className="border-1.5 rounded-md text-gray-900 hover:bg-slate-400/40 px-1"
                                             defaultValue={userId}
                                             onBlur={(e) => {
                                                 setUser(userName, e.target.value, userEmail).then().catch(e => console.log(e));
@@ -92,83 +90,83 @@ export default function Page() {
                                 </div>
                             </dl>
                         </div>
-                        <div>
-                            <h2 className="text-base font-semibold text-gray-900 leading-7">
-                                Language and dates
-                            </h2>
-                            <p className="mt-1 text-sm text-gray-500 leading-6">
-                                Choose what language and date format to use
-                                throughout your account.
-                            </p>
+                        {/*<div>*/}
+                        {/*    <h2 className="text-base font-semibold text-gray-900 leading-7">*/}
+                        {/*        Language and dates*/}
+                        {/*    </h2>*/}
+                        {/*    <p className="mt-1 text-sm text-gray-500 leading-6">*/}
+                        {/*        Choose what language and date format to use*/}
+                        {/*        throughout your account.*/}
+                        {/*    </p>*/}
 
-                            <dl className="mt-6 text-sm border-t border-gray-200 space-y-6 divide-y divide-gray-100 leading-6">
-                                <div className="pt-6 sm:flex">
-                                    <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                                        Language
-                                    </dt>
-                                    <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">
-                                        <div className="text-gray-900">
-                                            English
-                                        </div>
-                                        <button
-                                            type="button"
-                                            className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                        >
-                                            Update
-                                        </button>
-                                    </dd>
-                                </div>
-                                <div className="pt-6 sm:flex">
-                                    <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                                        Date format
-                                    </dt>
-                                    <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">
-                                        <div className="text-gray-900">
-                                            DD-MM-YYYY
-                                        </div>
-                                        <button
-                                            type="button"
-                                            className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                        >
-                                            Update
-                                        </button>
-                                    </dd>
-                                </div>
-                                <Switch.Group as="div" className="flex pt-6">
-                                    <Switch.Label
-                                        as="dt"
-                                        className="flex-none w-64 pr-6 font-medium text-gray-900"
-                                        passive
-                                    >
-                                        Automatic timezone
-                                    </Switch.Label>
-                                    <dd className="flex items-center justify-end flex-auto">
-                                        <Switch
-                                            checked={automaticTimezoneEnabled}
-                                            onChange={
-                                                setAutomaticTimezoneEnabled
-                                            }
-                                            className={classNames(
-                                                automaticTimezoneEnabled
-                                                    ? "bg-indigo-600"
-                                                    : "bg-gray-200",
-                                                "flex w-8 cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                            )}
-                                        >
-                                            <span
-                                                aria-hidden="true"
-                                                className={classNames(
-                                                    automaticTimezoneEnabled
-                                                        ? "translate-x-3.5"
-                                                        : "translate-x-0",
-                                                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                                                )}
-                                            />
-                                        </Switch>
-                                    </dd>
-                                </Switch.Group>
-                            </dl>
-                        </div>
+                        {/*    <dl className="mt-6 text-sm border-t border-gray-200 space-y-6 divide-y divide-gray-100 leading-6">*/}
+                        {/*        <div className="pt-6 sm:flex">*/}
+                        {/*            <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">*/}
+                        {/*                Language*/}
+                        {/*            </dt>*/}
+                        {/*            <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">*/}
+                        {/*                <div className="text-gray-900">*/}
+                        {/*                    English*/}
+                        {/*                </div>*/}
+                        {/*                <button*/}
+                        {/*                    type="button"*/}
+                        {/*                    className="font-semibold text-indigo-600 hover:text-indigo-500"*/}
+                        {/*                >*/}
+                        {/*                    Update*/}
+                        {/*                </button>*/}
+                        {/*            </dd>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="pt-6 sm:flex">*/}
+                        {/*            <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">*/}
+                        {/*                Date format*/}
+                        {/*            </dt>*/}
+                        {/*            <dd className="flex justify-between mt-1 gap-x-6 sm:mt-0 sm:flex-auto">*/}
+                        {/*                <div className="text-gray-900">*/}
+                        {/*                    DD-MM-YYYY*/}
+                        {/*                </div>*/}
+                        {/*                <button*/}
+                        {/*                    type="button"*/}
+                        {/*                    className="font-semibold text-indigo-600 hover:text-indigo-500"*/}
+                        {/*                >*/}
+                        {/*                    Update*/}
+                        {/*                </button>*/}
+                        {/*            </dd>*/}
+                        {/*        </div>*/}
+                        {/*        <Switch.Group as="div" className="flex pt-6">*/}
+                        {/*            <Switch.Label*/}
+                        {/*                as="dt"*/}
+                        {/*                className="flex-none w-64 pr-6 font-medium text-gray-900"*/}
+                        {/*                passive*/}
+                        {/*            >*/}
+                        {/*                Automatic timezone*/}
+                        {/*            </Switch.Label>*/}
+                        {/*            <dd className="flex items-center justify-end flex-auto">*/}
+                        {/*                <Switch*/}
+                        {/*                    checked={automaticTimezoneEnabled}*/}
+                        {/*                    onChange={*/}
+                        {/*                        setAutomaticTimezoneEnabled*/}
+                        {/*                    }*/}
+                        {/*                    className={classNames(*/}
+                        {/*                        automaticTimezoneEnabled*/}
+                        {/*                            ? "bg-indigo-600"*/}
+                        {/*                            : "bg-gray-200",*/}
+                        {/*                        "flex w-8 cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"*/}
+                        {/*                    )}*/}
+                        {/*                >*/}
+                        {/*                    <span*/}
+                        {/*                        aria-hidden="true"*/}
+                        {/*                        className={classNames(*/}
+                        {/*                            automaticTimezoneEnabled*/}
+                        {/*                                ? "translate-x-3.5"*/}
+                        {/*                                : "translate-x-0",*/}
+                        {/*                            "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"*/}
+                        {/*                        )}*/}
+                        {/*                    />*/}
+                        {/*                </Switch>*/}
+                        {/*            </dd>*/}
+                        {/*        </Switch.Group>*/}
+                        {/*    </dl>*/}
+                        {/*</div>*/}
                     </div>
                 </main>
             </div>
